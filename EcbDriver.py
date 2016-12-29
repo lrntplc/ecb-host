c05cd9dbd267006479a0406e4c08f454893fd7a2
Laurentiu Palcu <lpalcu@gmail.com>
2016-12-29 12:54:38 +0200

    Add GPLv2 license headers
    Signed-off-by: Laurentiu Palcu <lpalcu@gmail.com>

--- EcbDriver.py
+++ EcbDriver.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+#
+#  Foldable Electronic Chess Board Project
+#
+#  This is the driver for:
+#    * top half controller;
+#    * bottom half controller;
+#    * command panel controller;
+#
+#  Copyright 2016 - Laurentiu Palcu <lpalcu@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+
 import mraa
 from threading import Timer
 
--- ecb.py
+++ ecb.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+
+#
+#  Foldable Electronic Chess Board Project
+#
+#  This is the main file.
+#
+#  Copyright 2016 - Laurentiu Palcu <lpalcu@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+
 from EcbDriver import EcbDriver
 from EcbFSM import Ecb, Event
 import logging
